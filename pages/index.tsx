01c18f5eff3000416450160586be3547704ca745
Brendan McKenzie <brendan.mckenzie@versa.agency>
2020-08-25 19:01:54 +1000

    harryfied

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Document from "next/document";
+import { Logo } from "../components/Logo";
 
 const Home: React.FC<Document> = () => (
   <>
@@ -8,36 +9,47 @@ const Home: React.FC<Document> = () => (
       <link rel="icon" type="image/png" href="/pokko.png" />
     </Head>
     <main>
-      <div className="banner">
-        <div className="banner--left">
-          <img src="/pokko.png" style={{ width: 128 }} alt="Pokko" />
-          <h1>Pokko</h1>
-          <h3>A better way to manage your content.</h3>
-
-          <hr />
-
-          <span className="heading">Features</span>
-          <ul>
-            <li>Advanced data modelling</li>
-            <li>Content inheritance</li>
-            <li>Hierarchical content structure</li>
-            <li>Integrated webhooks</li>
-            <li>Localised data storage</li>
-            <li>
-              <em>... and more</em>
-            </li>
-          </ul>
+      <div className="container">
+        <div className="header">
+          <Logo />
+          <h1>Pokko is a better way to manage your content.</h1>
         </div>
-        <div className="banner--right">
-          <h2 className="heading">Register interest</h2>
-          <p>
-            To join our EAP (early access program), register your interest and
-            we'll be in touch with a user account.
-          </p>
-          <form action="">
-            <input type="email" placeholder="Email address" className="input" />
-            <button type="submit">Keep me posted</button>
-          </form>
+
+        <div className="columns">
+          <div className="column">
+            <p>
+              A content management system that features{" "}
+              <strong>advanced data modelling</strong>,
+              <strong>content inheritance</strong>,{" "}
+              <strong>hierarchical content structure</strong>,{" "}
+              <strong>integrated webhooks</strong>,{" "}
+              <strong>localised data storage</strong> and more soon to come.
+            </p>
+            <p>
+              You can contact us if you’re interested in learning more about our
+              product.
+            </p>
+            <p>
+              <a href="mailto:support@pokko.io">Contact us</a>
+            </p>
+          </div>
+          <div className="column">
+            <div className="panel">
+              <h2 className="heading">Register your interest</h2>
+              <p>
+                To join our EAP (early access program), register your interest
+                and we'll be in touch with a user account.
+              </p>
+
+              <input
+                type="email"
+                className="input"
+                placeholder="Email address"
+              />
+
+              <button>Keep me posted</button>
+            </div>
+          </div>
         </div>
       </div>
     </main>
