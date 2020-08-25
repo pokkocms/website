bddeb8e64be285cedd8c3790ce5e4e006040e2e0
Brendan McKenzie <brendan.mckenzie@versa.agency>
2020-08-26 08:32:48 +1000

    added metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,6 +50,19 @@ const Home: React.FC<Document> = () => (
     <Head>
       <title>Pokko - Content made better</title>
       <link rel="icon" type="image/png" href="/pokko.png" />
+      <meta
+        name="description"
+        content="Pokko - a modern solution to complex content management"
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Pokko" />
+      <meta
+        property="og:description"
+        content="Pokko - a modern solution to complex content management"
+      />
+      <meta property="og:image" content="https://www.pokko.io/pokko.png" />
+      <meta property="og:url" content="https://www.pokko.io/" />
+      <meta property="og:site_name" content="Pokko" />
     </Head>
     <main>
       <div className="container">
