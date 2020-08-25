2af9fec8ad1d3f66d776f81290549d73f341c493
Brendan McKenzie <brendan.mckenzie@versa.agency>
2020-08-25 20:39:21 +1000

    removed public url

--- pages/api/eap.ts
+++ pages/api/eap.ts
@@ -2,14 +2,11 @@ import { NowRequest, NowResponse } from "@vercel/node";
 import { Client } from "@hubspot/api-client";
 
 const handler = async (req: NowRequest, res: NowResponse) => {
-  fetch(
-    "https://hooks.slack.com/services/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",
-    {
-      method: "POST",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify({ text: `New contact! ${req.body.email}` }),
-    }
-  );
+  fetch(process.env.SLACK_URL, {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({ text: `New contact! ${req.body.email}` }),
+  });
 
   const hubspotClient = new Client({
     apiKey: process.env.HUBSPOT_API_KEY,
