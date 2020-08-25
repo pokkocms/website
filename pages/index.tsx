b20778e4edaee22ff5e9202f24de2a11ef6bcec4
Brendan McKenzie <brendan.mckenzie@versa.agency>
2020-08-26 08:37:31 +1000

    updated meta share image

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,7 +60,7 @@ const Home: React.FC<Document> = () => (
         property="og:description"
         content="Pokko - a modern solution to complex content management"
       />
-      <meta property="og:image" content="https://www.pokko.io/pokko.png" />
+      <meta property="og:image" content="https://www.pokko.io/pokko-sharing.png" />
       <meta property="og:url" content="https://www.pokko.io/" />
       <meta property="og:site_name" content="Pokko" />
     </Head>
