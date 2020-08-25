a1670d64535ea9971eae6b55fd9039a8ac2d4565
Brendan McKenzie <brendan.mckenzie@versa.agency>
2020-08-25 19:53:52 +1000

    debugging

--- pages/api/eap.ts
+++ pages/api/eap.ts
@@ -1,11 +1,13 @@
 import { NowRequest, NowResponse } from "@vercel/node";
 import { Client } from "@hubspot/api-client";
 
-const hubspotClient = new Client({
-  apiKey: process.env.HUBSPOT_API_KEY,
-});
-
 const handler = async (req: NowRequest, res: NowResponse) => {
+  console.log(Object.keys(process.env));
+
+  const hubspotClient = new Client({
+    apiKey: process.env.HUBSPOT_API_KEY,
+  });
+
   await hubspotClient.crm.contacts.basicApi.create({
     properties: {
       email: req.body.email,
