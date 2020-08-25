8cc946cfb5715a5a1062df05efd361a7de2085df
Brendan McKenzie <brendan.mckenzie@versa.agency>
2020-08-25 20:07:37 +1000

    slack notify

--- pages/api/eap.ts
+++ pages/api/eap.ts
@@ -1,15 +1,23 @@
 import { NowRequest, NowResponse } from "@vercel/node";
 import { Client } from "@hubspot/api-client";
+import { WebClient } from "@slack/web-api";
 
 const handler = async (req: NowRequest, res: NowResponse) => {
   const hubspotClient = new Client({
     apiKey: process.env.HUBSPOT_API_KEY,
   });
 
+  const slackClient = new WebClient(process.env.SLACK_TOKEN);
+
   await hubspotClient.crm.contacts.basicApi.create({
     properties: {
       email: req.body.email,
     },
+  });
+
+  await slackClient.chat.postMessage({
+    channel: "C019C13LGKE", // #sales
+    text: `New contact! ${req.body.email}`,
   });
 
   res.json({ success: true });
