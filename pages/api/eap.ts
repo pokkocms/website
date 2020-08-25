efb908897fa87cfc8637d2b0437e8beca7911edb
Brendan McKenzie <brendan.mckenzie@versa.agency>
2020-08-25 20:32:34 +1000

    hmm

--- pages/api/eap.ts
+++ pages/api/eap.ts
@@ -2,6 +2,15 @@ import { NowRequest, NowResponse } from "@vercel/node";
 import { Client } from "@hubspot/api-client";
 
 const handler = async (req: NowRequest, res: NowResponse) => {
+  fetch(
+    "https://hooks.slack.com/services/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",
+    {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ text: `New contact! ${req.body.email}` }),
+    }
+  );
+
   const hubspotClient = new Client({
     apiKey: process.env.HUBSPOT_API_KEY,
   });
@@ -11,15 +20,6 @@ const handler = async (req: NowRequest, res: NowResponse) => {
       email: req.body.email,
     },
   });
-
-  fetch(
-    "https://hooks.slack.com/services/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",
-    {
-      method: "POST",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify({ text: `New contact! ${req.body.email}` }),
-    }
-  );
 
   res.json({ success: true });
 };
