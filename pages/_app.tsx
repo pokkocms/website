f7bf3706f6444546fe028977720144c457bc9d48
Brendan McKenzie <brendan.mckenzie@versa.agency>
2020-08-31 09:33:42 +1000

    added ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
+import * as React from "react";
 import "../styles/globals.css";
 import { AppProps } from "next/app";
+import Head from "next/head";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <>
+    <Head>
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=UA-176777547-1`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-176777547-1');
+          `,
+        }}
+      />
+    </Head>
+    <Component {...pageProps} />
+  </>
 );
 
 export default App;
