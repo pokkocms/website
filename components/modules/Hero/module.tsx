a53ce36dff7a1b87cfdd45b4d40216e0e7b07449
Brendan McKenzie <brendan@pokko.io>
2020-12-18 08:57:46 +1100

    add form to hero

--- components/modules/Hero/module.tsx
+++ components/modules/Hero/module.tsx
@@ -1,14 +1,18 @@
 import * as React from "react";
+import { useForm } from "react-hook-form";
 import ReactMarkdown from "react-markdown";
 import { HeroModuleFragment } from "../../../api/queries";
+import { useRegisterForm, RegisterFormInput } from "../../../hooks/form";
 import { Navbar } from "../../Navbar/module";
 
 export const HeroModule: React.FC<HeroModuleFragment> = ({
   title,
-  link,
   heroBody,
   image,
 }) => {
+  const [submit, { state, message }] = useRegisterForm();
+  const { register, handleSubmit, errors } = useForm<RegisterFormInput>();
+
   return (
     <div className="hero__container">
       <Navbar />
@@ -21,9 +25,34 @@ export const HeroModule: React.FC<HeroModuleFragment> = ({
           </h1>
           <p>{heroBody}</p>
 
-          {link.__typename === "Link" ? (
-            <a href={link.target}>{link.text}</a>
-          ) : null}
+          {state === "success" ? (
+            <div className="eap-basic__success">
+              <p>{message ?? "Thank you!"}</p>
+            </div>
+          ) : (
+            <form className="eap-basic__form" onSubmit={handleSubmit(submit)}>
+              <div className="control">
+                <input
+                  type="email"
+                  placeholder="Your email"
+                  className={errors.email ? "input --danger" : "input"}
+                  name="email"
+                  ref={register({ required: "This field is required" })}
+                />
+
+                {errors.email ? (
+                  <p className="message --danger">{errors.email.message}</p>
+                ) : null}
+              </div>
+              <button
+                type="submit"
+                className="button"
+                disabled={state === "loading"}
+              >
+                Register now
+              </button>
+            </form>
+          )}
         </div>
         <div className="hero__right">
           <img src={image?.url} />
