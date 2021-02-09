ad9503573f103ea569ecc23f854233bf170b1b7e
Brendan McKenzie <brendan@pokko.io>
2021-02-10 08:28:52 +1100

    update robots for preview site

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,19 @@
 module.exports = {
   siteUrl: "https://www.pokko.io",
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: process.env.PREVIEW
+      ? [
+          {
+            userAgent: "*",
+            disallow: "/",
+          },
+        ]
+      : [
+          {
+            userAgent: "*",
+            allow: "/",
+          },
+        ],
+  },
 };
