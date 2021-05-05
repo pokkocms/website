d23a5237d72941fb35bd91a59a4a1552aeed9de0
Brendan McKenzie <brendan@pokko.io>
2021-05-05 19:35:35 +1000

    fix path reference

--- components/modules/Banner/preview.tsx
+++ components/modules/Banner/preview.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import { Banner } from "./module";
 
 export const PreviewBanner: React.FC = () => {
-  const { isPreview, pathname } = useRouter();
+  const { isPreview, asPath } = useRouter();
 
   if (!isPreview) {
     return null;
@@ -29,7 +29,7 @@ export const PreviewBanner: React.FC = () => {
         <>
           <p>
             You are viewing this site in Preview mode.{" "}
-            <a href={`/api/preview?mode=public&path=${pathname}`}>
+            <a href={`/api/preview?mode=public&path=${asPath}`}>
               Click here to exit
             </a>
             .
