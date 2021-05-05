760c1bf2d3359d7e3f514eedcf3b161b0d7923b6
Brendan McKenzie <brendan@pokko.io>
2021-05-05 19:23:47 +1000

    handling preview urls

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -1,6 +1,23 @@
 import { NextApiHandler } from "next";
 
 const handler: NextApiHandler = (req, res) => {
+  if (req.query.mode && req.query.path) {
+    switch (req.query.mode) {
+      case "public":
+        res.clearPreviewData();
+        res.redirect(302, req.query.path as string);
+        return;
+      case "preview":
+        res.setPreviewData({});
+        res.redirect(302, req.query.path as string);
+        return;
+    }
+
+    res.send("Bad request");
+    res.status(400).end();
+    return;
+  }
+
   if (req.query.cancel) {
     res.clearPreviewData();
     res.end("Preview mode disabled");
