4af4f6f004c572aded6b420cf798e53f0376a29f
Brendan McKenzie <brendan@pokko.io>
2021-05-05 19:38:26 +1000

    banner colours

--- components/modules/Banner/module.tsx
+++ components/modules/Banner/module.tsx
@@ -3,10 +3,11 @@ import * as React from "react";
 export type BannerProps = {
   icon?: React.ReactNode;
   body: React.ReactNode;
+  colour?: "grey";
 };
 
-export const Banner: React.FC<BannerProps> = ({ icon, body }) => (
-  <div className="banner__container">
+export const Banner: React.FC<BannerProps> = ({ icon, body, colour }) => (
+  <div className={"banner__container" + (colour ? ` --${colour}` : "")}>
     {icon ? <div className="banner__icon">{icon}</div> : null}
     <div className="banner__body">{body}</div>
   </div>
